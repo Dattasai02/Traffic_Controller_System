f6e7165c1340fce619c5e0124828893686dbdf33
Dattasai02 <105917549+Dattasai02@users.noreply.github.com>
2023-10-10 14:15:31 +0530

    updated selenium
    Minor changes

--- Traffic_Controller_System/src/test/java/com/src/mn/SelTest.java
+++ Traffic_Controller_System/src/test/java/com/src/mn/SelTest.java
@@ -1,33 +1,20 @@
 package com.src.mn;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
+
+import com.src.services.AddTrafficTest;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SelTest {
-	
-	static String browser = "edge";
-	static WebDriver driver;
-
 	public static void main(String[] args) {
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
 		
-		if (browser.equals("chrome")) {
-			//WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-			driver.get("https://www.facebook.com/");
-
-		} else if (browser.equals("edge")) {
-			WebDriverManager.edgedriver().setup();
-			EdgeOptions options = new EdgeOptions();
-			options.addArguments("--disable-popup-blocking");
-			driver = new EdgeDriver(options);
-			driver.get("https://www.facebook.com/");
-		}
-	
+		AddTrafficTest at = new AddTrafficTest();
+		at.addTraffic(driver);
 		
-
 	}
 
 }
